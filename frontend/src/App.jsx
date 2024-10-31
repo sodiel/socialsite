318263b1d100d73817406263bd0a3dfdac781d83
sodiel <detopuse@gmail.com>
2024-10-31 14:12:21 +0300

    add tailwind, теперь точно, ну почти

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <>
       <div>
         <a href="https://vite.dev" target="_blank">

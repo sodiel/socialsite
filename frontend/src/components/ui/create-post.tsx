16320b8b774fbbf8d5aa88f4240912860b63f60f
sodiel <detopuse@gmail.com>
2024-12-28 13:17:35 +0300

    вкладки, создать пост

---
+++ frontend/src/components/ui/create-post.tsx
@@ -0,0 +1,47 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import React from "react";
+import { Input } from "./input";
+import { Label } from "./label";
+import { Textarea } from "./textarea";
+import { Button } from "./button";
+import { Upload } from "lucide-react";
+
+
+interface CreatePostProps {
+  visible?: boolean;
+}
+
+const CreatePost: React.FC<CreatePostProps> = () => {
+  return (
+    <div className="">
+      <Dialog>
+        <DialogTrigger className="block bg-slate-950 text-slate-100">Создать пост</DialogTrigger>
+
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Создать пост</DialogTitle>
+            <Label htmlFor="header">Заголовок</Label>
+            <Input id="header" placeholder="Введите заголовок" type="text"></Input>
+            <Button className=""><Upload /> Добавить картинку</Button> {/*тут надо бы кнопку сделать не на всю ширину*/}
+            <Label htmlFor="content">Контент</Label>
+            <Textarea className="h-10 resize-none" id="content" placeholder="Введите контент"></Textarea>
+            <div className="inline-block space-x-2">
+              <Button >Опубликовать пост</Button>
+              <Button variant="secondary">Отправить в черновики</Button>
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export { CreatePost };
---
+++ frontend/src/pages/feed/index.tsx
@@ -0,0 +1,97 @@
+<<<<<<< Updated upstream
+=======
+import * as React from "react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Post } from "@/components/ui/post";
+import { CreatePost } from "@/components/ui/create-post";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Label } from "@/components/ui/label";
+import { InputField } from "@/components/ui/input-field";
+
+export function Feed() {
+  const author: string = "pochta@gmail.com";
+  const date: string = "5 декабря";
+
+  return (
+    <>
+      <div
+        id="header"
+        className="z-10 bg-slate-50 top-0 left-0 flex w-full fixed"
+ >
+        <img alt="ЦПТ" src="/src/assets/logo.svg" className="w-20 ml-4" />
+        <div id="account" className="flex ml-auto mr-4 mt-2">
+          <p className="mr-1">{author}</p>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+      </div>
+      <div className="fixed z-10  w-full bg-slate-100 pt-0">
+      <div className="w-full mt-4 flex justify-start">
+        <Tabs defaultValue="all_posts" className="">
+          <TabsList>
+            <TabsTrigger value="all_posts">Все посты</TabsTrigger>
+            <TabsTrigger value="my_posts">Мои посты</TabsTrigger>
+            <TabsTrigger value="drafts">Черновики</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+      <div className="w-[100%]">
+      <CreatePost></CreatePost>
+      </div>
+      </div>
+
+
+      <div id="posts" className="flex-col max-w-[600px] mt-[100px]">
+        {[...Array(5)].map(() => (
+          <Post
+            author={author}
+            date={date}
+            header="Созвон ЦПТ"
+            content="Созвон ЦПТ"
+            type="draft"
+            likes={10}
+            comments={12}
+          ></Post>
+        ))}
+
+        <Post
+          author={author}
+          date={date}
+          header="Привет от Гарбуза"
+          content="Вопрос о ереси «жидовствующих» до сих пор остается дискуссионным бла бла бла ой ой ой ой ой ой ой о йо йо йой йой о а перенос будет? переноса строки нет!а нет есть"
+          type="editable"
+          likes={10}
+          comments={12}
+        ></Post>
+
+        <Post
+          author={author}
+          date={date}
+          header="Lorem ipsum"
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod idem licet transferre in voluptatem, ut postea variari voluptas distinguique possit, augeri amplificarique non possit. At etiam Athenis, ut e patre audiebam facete.
+        
+Ullus investigandi veri, nisi inveneris, et quaerendi defatigatio turpis est, cum esset accusata et vituperata ab Hortensio. Qui liber cum et mortem contemnit, qua qui est imbutus quietus esse numquam potest. Praeterea bona praeterita grata recordatione renovata delectant. Est autem situm in nobis ut et voluptates.
+
+Ullus investigandi veri, nisi inveneris, et quaerendi defatigatio turpis est, cum esset accusata et vituperata ab Hortensio. Qui liber cum et mortem contemnit, qua qui est imbutus quietus esse numquam potest. Praeterea bona praeterita grata recordatione renovata delectant. Est autem situm in nobis."
+          type="common"
+          likes={10}
+          comments={12}
+        ></Post>
+      </div>
+    </>
+  );
+}
+>>>>>>> Stashed changes

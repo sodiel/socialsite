1d37773e671f3c62c74cf003571c63ac81648327
sodiel <detopuse@gmail.com>
2025-01-15 23:32:15 +0300

    fix imports. again.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
